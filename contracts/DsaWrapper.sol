e6772f37ffd22ee57debf7c7290a677aae33ae4c
Chinmay Chougaonkar <chinmay0402@gmail.com>
2022-02-17 14:55:01 +0530

    added deposit/withdraw erc20 and add/remove authority functionalities with success tests

--- contracts/DsaWrapper.sol
+++ contracts/DsaWrapper.sol
@@ -7,10 +7,13 @@ import "./interfaces/Authority.sol";
 import "./interfaces/IDSA.sol";
 
 contract DsaWrapper {
+    using SafeERC20 for IERC20;
 
     address private owner;
-    address private authorityContractAddress = 0x351Bb32e90C35647Df7a584f3c1a3A0c38F31c68;
-    address private instapoolV2ContractAddress = 0x621AD080ad3B839e7b19e040C77F05213AB71524;
+    address private authorityContractAddress =
+        0x351Bb32e90C35647Df7a584f3c1a3A0c38F31c68;
+    address private instapoolV2ContractAddress =
+        0x621AD080ad3B839e7b19e040C77F05213AB71524;
     InstapoolV2 instapool = InstapoolV2(instapoolV2ContractAddress); // instantiate instapool contract
 
     constructor() {
@@ -21,17 +24,23 @@ contract DsaWrapper {
         return owner;
     }
 
-    function getAuthority(uint256 _id) view external returns (address[] memory) {
-        // get dsa-address from id        
+    function getAuthority(uint256 _id)
+        external
+        view
+        returns (address[] memory)
+    {
+        // get dsa-address from id
         address dsaAddress = instapool.getAccountIdDetails(_id).account;
 
-        address[] memory accountAuthorities = instapool.getAccountAuthorities(dsaAddress);
+        address[] memory accountAuthorities = instapool.getAccountAuthorities(
+            dsaAddress
+        );
 
         return accountAuthorities;
     }
 
     // deposit ether to dsa
-    function depositEtherToDsa(uint256 _id) external payable {
+    function depositEther(uint256 _id) external payable {
         // get dsa-address
         address dsaAddress = instapool.getAccountIdDetails(_id).account;
 
@@ -39,11 +48,19 @@ contract DsaWrapper {
         targets[0] = "BASIC-A";
 
         bytes[] memory data = new bytes[](1);
-        bytes4 basicDeposit = bytes4(keccak256("deposit(address,uint256,uint256,uint256)"));
+        bytes4 basicDeposit = bytes4(
+            keccak256("deposit(address,uint256,uint256,uint256)")
+        );
 
         address tokenAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-        data[0] = abi.encodeWithSelector(basicDeposit, tokenAddress, msg.value, 0, 0);
+        data[0] = abi.encodeWithSelector(
+            basicDeposit,
+            tokenAddress,
+            msg.value,
+            0,
+            0
+        );
 
         IDSA(dsaAddress).cast{value: msg.value}(targets, data, address(0));
     }
@@ -56,17 +73,104 @@ contract DsaWrapper {
         targets[0] = "BASIC-A";
 
         bytes[] memory data = new bytes[](1);
-        bytes4 basicWithdraw = bytes4(keccak256("withdraw(address,uint256,address,uint256,uint256)"));
+        bytes4 basicWithdraw = bytes4(
+            keccak256("withdraw(address,uint256,address,uint256,uint256)")
+        );
 
         address tokenAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-        data[0] = abi.encodeWithSelector(basicWithdraw, tokenAddress, _amt, msg.sender, 0, 0);
+        data[0] = abi.encodeWithSelector(
+            basicWithdraw,
+            tokenAddress,
+            _amt,
+            msg.sender,
+            0,
+            0
+        );
+
+        IDSA(dsaAddress).cast(targets, data, address(0));
+    }
+
+    function depositErc20(
+        uint256 _id,
+        uint256 _amt,
+        address _tokenAddress
+    ) external payable {
+        IERC20 token = IERC20(_tokenAddress);
+
+        // get dsa-address
+        address dsaAddress = instapool.getAccountIdDetails(_id).account;
+
+        token.safeTransferFrom(msg.sender, address(this), _amt);
+        token.safeApprove(dsaAddress, _amt);
+
+        string[] memory targets = new string[](1);
+        targets[0] = "BASIC-A";
+
+        bytes[] memory data = new bytes[](1);
+        bytes4 basicDeposit = bytes4(
+            keccak256("deposit(address,uint256,uint256,uint256)")
+        );
+
+        data[0] = abi.encodeWithSelector(
+            basicDeposit,
+            _tokenAddress,
+            _amt,
+            0,
+            0
+        );
+        IDSA(dsaAddress).cast(targets, data, address(0));
+    }
+
+    // withdraw ether from dsa
+    function withdrawErc20(uint256 _id, uint256 _amt, address _tokenAddress) external {
+        address dsaAddress = instapool.getAccountIdDetails(_id).account;
+
+        string[] memory targets = new string[](1);
+        targets[0] = "BASIC-A";
+
+        bytes[] memory data = new bytes[](1);
+        bytes4 basicWithdraw = bytes4(
+            keccak256("withdraw(address,uint256,address,uint256,uint256)")
+        );
+
+        data[0] = abi.encodeWithSelector(
+            basicWithdraw,
+            _tokenAddress,
+            _amt,
+            msg.sender,
+            0,
+            0
+        );
 
         IDSA(dsaAddress).cast(targets, data, address(0));
     }
 
     // add authority
-    
-    // remove authority
+    function addAuthority(uint256 _id, address _authority) external {
+        address dsaAddress = instapool.getAccountIdDetails(_id).account;
 
+        string[] memory target = new string[](1);
+        target[0] = "AUTHORITY-A";
+
+        bytes[] memory data = new bytes[](1);
+        bytes4 addAuth = bytes4(keccak256("add(address)"));
+
+        data[0] = abi.encodeWithSelector(addAuth, _authority);
+        IDSA(dsaAddress).cast(target, data, address(0));
+    }
+
+    // remove authority
+    function removeAuthority(uint256 _id, address _authority) external {
+        address dsaAddress = instapool.getAccountIdDetails(_id).account;
+
+        string[] memory target = new string[](1);
+        target[0] = "AUTHORITY-A";
+
+        bytes[] memory data = new bytes[](1);
+        bytes4 removeAuth = bytes4(keccak256("remove(address)"));
+
+        data[0] = abi.encodeWithSelector(removeAuth, _authority);
+        IDSA(dsaAddress).cast(target, data, address(0));
+    }
 }
--- test/dsa-wrapper-tests.js
+++ test/dsa-wrapper-tests.js
@@ -1,15 +1,17 @@
 const { expect, should } = require("chai");
 require('hardhat');
 const DSA = require('dsa-connect');
-const { parseEther } = require("ethers/lib/utils");
+const { parseEther, parseUnits } = require("ethers/lib/utils");
 require("@nomiclabs/hardhat-web3");
-const dsaContractAbi = require("./dsaContractAbi.js");
+const dsaContractAbi = require("./ABIs/dsaContractAbi.js");
+const daiAbi = require("./ABIs/daiAbi.js");
 
 describe("DSA Wrapper Contract", function () {
-  let owner;
-  let dsaWrapper;
+  let owner, addr1;
+  let dsaWrapper, dai;
   let dsa, dsaId, dsaAddress;
   const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+  const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 
   dsa = new DSA({
     web3: web3,
@@ -33,7 +35,7 @@ describe("DSA Wrapper Contract", function () {
     dsaId = accounts[0].id;
 
     const dsaWrapperFactory = await ethers.getContractFactory("DsaWrapper");
-    [owner] = await ethers.getSigners();
+    [owner, addr1] = await ethers.getSigners();
     dsaWrapper = await dsaWrapperFactory.deploy();
 
     await dsa.setInstance(dsaId);
@@ -47,23 +49,27 @@ describe("DSA Wrapper Contract", function () {
     const transactionHash = await spells.cast({
       gasPrice: web3.utils.toWei('1000000', 'gwei'), // in gwei, used in node implementation.
     });
+
+    // instantiate Dai Token
+    dai = new ethers.Contract(daiAddress, daiAbi, ethers.provider);
+    await dai.connect(owner).approve(dsaWrapper.address, parseUnits('1', 18));
   });
 
   describe('Deployment', async () => {
     it('Sets the correct owner', async () => {
-      expect(await dsaWrapper.getOwner()).to.equal(owner.address);
+      await expect(await dsaWrapper.getOwner()).to.equal(owner.address);
     });
   });
 
   describe('Authority', async () => {
     it('should return correct account authority', async () => {
-      expect(await dsaWrapper.getAuthority(dsaId)).to.have.all.members([owner.address, dsaWrapper.address]);
+      await expect(await dsaWrapper.getAuthority(dsaId)).to.have.all.members([owner.address, dsaWrapper.address]);
     });
   });
 
   describe('Deposit Ether', async () => {
     it('Should deposit Ether to DSA and update balances', async () => {
-      expect(await dsaWrapper.depositEtherToDsa(dsaId, {
+      await expect(await dsaWrapper.depositEther(dsaId, {
         value: parseEther('1')
       }))
         .to.changeEtherBalances([owner, dsaContract], [parseEther('-1'), parseEther('1')]);
@@ -72,23 +78,48 @@ describe("DSA Wrapper Contract", function () {
 
   describe('Withdraw Ether', async () => {
     it('Should withdraw Ether from DSA and update balances (1)', async () => {
-      expect(await dsaWrapper.withdrawEther(dsaId, parseEther('0.6')))
+      await expect(await dsaWrapper.withdrawEther(dsaId, parseEther('0.6')))
         .to.changeEtherBalances([owner, dsaContract], [parseEther('0.6'), parseEther('-0.6')]);
     });
 
+    // add test for withdraw failure
+
     it('Should withdraw Ether from DSA and update balances (2)', async () => {
-      expect(await dsaWrapper.withdrawEther(dsaId, parseEther('0.4')))
+      await expect(await dsaWrapper.withdrawEther(dsaId, parseEther('0.4')))
         .to.changeEtherBalances([owner, dsaContract], [parseEther('0.4'), parseEther('-0.4')]);
+    });
+  });
+
+  describe('Deposit ERC20', async () => {
+    it('Should deposit ERC20 to DSA and update balances', async () => {
+      await expect(() => dsaWrapper.depositErc20(dsaId, parseUnits('1', 18), daiAddress))
+        .to.changeTokenBalances(dai, [owner, dsaContract], [parseUnits('-1', 18), parseUnits('1', 18)]);
+    });
+  });
+
+  describe('Withdraw ERC20', async () => {
+    it('Should withdraw ERC20 from DSA and update balances', async () => {
+      await expect(() => dsaWrapper.withdrawErc20(dsaId, parseUnits('0.4', 18), daiAddress))
+        .to.changeTokenBalances(dai, [owner, dsaContract], [parseUnits('0.4', 18), parseUnits('-0.4', 18)]);
     });
   });
 
   describe('Authority', async () => {
     it('Should add authority', async () => {
-      
+      await dsaWrapper.addAuthority(dsaId, addr1.address);
+
+      await expect(await dsaWrapper.getAuthority(dsaId)).to.have.all.members([owner.address, dsaWrapper.address, addr1.address]);
     });
 
     it('Should remove authority', async () => {
+      // to confirm if addr1 is still auth
+      await expect(await dsaWrapper.getAuthority(dsaId)).to.have.all.members([owner.address, dsaWrapper.address, addr1.address]);
+      
+      // remove addr1 from auth
+      await dsaWrapper.removeAuthority(dsaId, addr1.address);
 
+      // check if remove was successful
+      await expect(await dsaWrapper.getAuthority(dsaId)).to.have.all.members([owner.address, dsaWrapper.address]);
     });
   });
 });
